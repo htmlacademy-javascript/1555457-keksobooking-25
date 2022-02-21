33495867c4cdf31333aa81e39c091b9875a7fb1e
Раиса Пыхтина <raisapyhtina@Mac-mini-Raisa.local>
2022-02-21 16:07:54 +0300

    v2

--- js/main.js
+++ js/main.js
@@ -1,23 +1,29 @@
 function getRandomNumber (min, max) {
-    if (min >= max) {
-        return min;
-    }
-    if (min < 0 || max < 0) {
-        return null;
-    }
-    min = Math.floor(min);
-    max = Math.ceil(max);
+  if (min > max) {
+    const SWAP = min;
+    min = max;
+    max = SWAP;
+  }
+  if (min < 0 || max < 0) {
+    return null;
+  }
+  min = Math.floor(min);
+  max = Math.ceil(max);
 
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-    //source: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+  //source: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 }
 function getRandomFloat (min, max, digits) {
-    if (min >= max) {
-        return min;
-    }
-    if (min < 0 || max < 0) {
-        return null;
-    }
+  if (min > max) {
+    const SWAP = min;
+    min = max;
+    max = SWAP;
+  }
+  if (min < 0 || max < 0) {
+    return null;
+  }
 
-    return (Math.random() * (max - min + 1) + min).toFixed(digits);
+  return (Math.random() * (max - min + 1) + min).toFixed(digits);
 }
+getRandomFloat();
+getRandomNumber();
