df3e9d8aa18622c5bd2b92e9d3da78701787f68d
Раиса Пыхтина <raisapyhtina@Mac-mini-Raisa.local>
2022-02-21 13:38:12 +0300

    Задание 2

---
+++ js/main.js
@@ -0,0 +1,23 @@
+function getRandomNumber (min, max) {
+    if (min >= max) {
+        return min;
+    }
+    if (min < 0 || max < 0) {
+        return null;
+    }
+    min = Math.floor(min);
+    max = Math.ceil(max);
+
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+    //source: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+}
+function getRandomFloat (min, max, digits) {
+    if (min >= max) {
+        return min;
+    }
+    if (min < 0 || max < 0) {
+        return null;
+    }
+
+    return (Math.random() * (max - min + 1) + min).toFixed(digits);
+}
