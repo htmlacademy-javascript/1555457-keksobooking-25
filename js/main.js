4d61f48ffd6be9dede3ecac173ae9d3282961954
arrowls <97401697+arrowls@users.noreply.github.com>
2022-03-04 20:30:08 +0300

    Исправлены названия массивов и доработаны функции

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ function getRandomNumber (min, max) {
   min = Math.floor(min);
   max = Math.ceil(max);
 
-  return Math.floor(Math.random() * (max - min)) + min;
+  return Math.round(Math.random() * (max - min)) + min;
 }
 function getRandomFloat (min, max, digits) {
   if (min > max) {
@@ -23,7 +23,7 @@ function getRandomFloat (min, max, digits) {
   }
   return (Math.random() * (max - min) + min).toFixed(digits);
 }
-const hotelTitles = [
+const HOTEL_TITLES = [
   'Шикарный апарт-отель',
   'Дом в Геленджике',
   'Уютный номер с видом на море',
@@ -35,19 +35,19 @@ const hotelTitles = [
   'Номер на двоих',
   'Номер на двоих'
 ];
-const hotelTypes = [
+const HOTEL_TYPES = [
   'palace',
   'flat',
   'house',
   'bungalow',
   'hotel'
 ];
-const checkinOptions = [
+const CHECKIN_OPTIONS = [
   '12:00',
   '13:00',
   '14:00'
 ];
-const hotelFeatures = [
+const HOTEL_FEATURES = [
   'wifi',
   'dishwasher',
   'parking',
@@ -55,7 +55,7 @@ const hotelFeatures = [
   'elevator',
   'conditioner'
 ];
-const hotelPhotos = [
+const HOTEL_PHOTOS = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
@@ -76,22 +76,22 @@ function createLandingArray () {
     temp.location.lng = +getRandomFloat(139.70000, 139.80000, 5);
 
     temp.offer.address = `${temp.location.lat}, ${temp.location.lng}`;
-    temp.offer.title = hotelTitles[i-1];
+    temp.offer.title = HOTEL_TITLES[i-1];
     temp.offer.price = getRandomNumber(2000, 15000);
-    temp.offer.type = hotelTypes[getRandomNumber(0, hotelTypes.length - 1)];
+    temp.offer.type = HOTEL_TYPES[getRandomNumber(0, HOTEL_TYPES.length - 1)];
     temp.offer.rooms = getRandomNumber(1, 5);
     temp.offer.guests = getRandomNumber(1, 15);
-    temp.offer.checkin = checkinOptions[getRandomNumber(0, checkinOptions.length)];
-    temp.offer.checkout = checkinOptions[getRandomNumber(0, checkinOptions.length)];
-    temp.offer.features = getRandomElements(hotelFeatures);
+    temp.offer.checkin = CHECKIN_OPTIONS[getRandomNumber(0, CHECKIN_OPTIONS.length - 1)];
+    temp.offer.checkout = CHECKIN_OPTIONS[getRandomNumber(0, CHECKIN_OPTIONS.length - 1)];
+    temp.offer.features = getRandomElements(HOTEL_FEATURES);
     temp.offer.description = 'Отель сочетает в себе уют домашнего очага и комфорт современной обстановки. Все номера выходят окнами на тихий закрытый двор, оснащены стеклопакетами и кондиционерами, имеют отдельную туалетную комнату с душевой кабиной и феном.';
-    temp.offer.photos = getRandomElements(hotelPhotos);
+    temp.offer.photos = getRandomElements(HOTEL_PHOTOS);
 
     landingArray.push(temp);
   }
 
   function getRandomElements (array) {
-    const elementsNumber = getRandomNumber(1, array.length);
+    const elementsNumber = getRandomNumber(1, array.length - 1);
     const tempArray = [];
     for (let i = 0; i < elementsNumber; i++) {
       tempArray.push(array[getRandomNumber(1, array.length - 1)]);
